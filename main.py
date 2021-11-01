a9c8b38dc3cdd1a2a70be6f2b044a1d571a60727
AlexEMG <alexander@deeplabcut.org>
2021-11-01 10:04:07 +0100

    proper imports

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Code adapted from https://scipy-cookbook.readthedocs.io/items/LoktaVolterraTutorial.html
-from numpy import *
-
+import numpy as np
+from scipy import integrate
 
 # Definition of parameters
 a = 1.        # natural growth rate of rabits (prey)
@@ -10,10 +10,11 @@ d = 0.75      # factor describing growth of foxes based on caught rabbits
 
 def dX_dt(X, t=0):
     """ Return the growth rate of fox and rabbit populations. """
-    return array([ a*X[0] -   b*X[0]*X[1] ,
+    return np.array([ a*X[0] -   b*X[0]*X[1] ,
                   -c*X[1] + d*b*X[0]*X[1] ])
 
-from scipy import integrate
-t = linspace(0, 15,  1000)              # time
-X0 = array([10, 5])                     # initials conditions: 10 rabbits and 5 foxes
+t = np.linspace(0, 15,  1000)              # time
+X0 = np.array([10, 5])                     # initials conditions: 10 rabbits and 5 foxes
 X, infodict = integrate.odeint(dX_dt, X0, t, full_output=True)
+
+print(X)
