c3db6e768c9768f9d6b1640deaa51bf875b2ad3c
AlexEMG <alexander@deeplabcut.org>
2021-11-01 18:36:07 +0100

    black formatting

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import numpy as np
 from scipy import integrate
 
 # Definition of parameters
-a = 1.        # natural growth rate of rabits (prey)
-b = 0.1       # natural dying rate of rabbits
-c = 1.5       # natural dyring rate of foxes
-d = 0.75      # factor describing growth of foxes based on caught rabbits
+a = 1.0  # natural growth rate of rabits (prey)
+b = 0.1  # natural dying rate of rabbits
+c = 1.5  # natural dyring rate of foxes
+d = 0.75  # factor describing growth of foxes based on caught rabbits
+
 
 def dX_dt(X, t=0):
     """ Return the growth rate of fox and rabbit populations. """
-    return np.array([ a*X[0] -   b*X[0]*X[1] ,
-                  -c*X[1] + d*b*X[0]*X[1] ])
+    return np.array([a * X[0] - b * X[0] * X[1], -c * X[1] + d * b * X[0] * X[1]])
 
-t = np.linspace(0, 15,  1000)              # time
-X0 = np.array([10, 5])                     # initials conditions: 10 rabbits and 5 foxes
+
+t = np.linspace(0, 15, 1000)  # time
+X0 = np.array([10, 5])  # initials conditions: 10 rabbits and 5 foxes
 X, infodict = integrate.odeint(dX_dt, X0, t, full_output=True)
 
 print(X)
