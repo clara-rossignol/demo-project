0dd4d38d09305d67917a173fe4b3a9e1e14ff616
AlexEMG <alexander@deeplabcut.org>
2021-11-01 10:03:03 +0100

    basic code

---
+++ main.py
@@ -0,0 +1,19 @@
+# Code adapted from https://scipy-cookbook.readthedocs.io/items/LoktaVolterraTutorial.html
+from numpy import *
+
+
+# Definition of parameters
+a = 1.        # natural growth rate of rabits (prey)
+b = 0.1       # natural dying rate of rabbits
+c = 1.5       # natural dyring rate of foxes
+d = 0.75      # factor describing growth of foxes based on caught rabbits
+
+def dX_dt(X, t=0):
+    """ Return the growth rate of fox and rabbit populations. """
+    return array([ a*X[0] -   b*X[0]*X[1] ,
+                  -c*X[1] + d*b*X[0]*X[1] ])
+
+from scipy import integrate
+t = linspace(0, 15,  1000)              # time
+X0 = array([10, 5])                     # initials conditions: 10 rabbits and 5 foxes
+X, infodict = integrate.odeint(dX_dt, X0, t, full_output=True)
