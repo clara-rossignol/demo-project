10b24d1eb1f5bc1880ad3d8c206fc6b76280ccf9
Alberto Chiappa <alberto.chiappa@epfl.ch>
2021-11-04 10:43:40 +0100

    Use the system equation from the functions module in the main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Code adapted from https://scipy-cookbook.readthedocs.io/items/LoktaVolterraTutorial.html
 import numpy as np
 from scipy import integrate
+from functions import dX_dt
+
 
 # Definition of parameters
 a = 1.0  # natural growth rate of rabbits (prey)
@@ -8,14 +10,8 @@ b = 0.1  # natural dying rate of rabbits
 c = 1.5  # natural dying rate of foxes
 d = 0.75  # factor describing growth of foxes based on caught rabbits
 
-
-def dX_dt(X, t=0):
-    """ Return the growth rate of fox and rabbit populations. """
-    return np.array([a * X[0] - b * X[0] * X[1], -c * X[1] + d * b * X[0] * X[1]])
-
-
 t = np.linspace(0, 15, 1000)  # time
-X0 = np.array([10, 5])  # initials conditions: 10 rabbits and 5 foxes
-X, infodict = integrate.odeint(dX_dt, X0, t, full_output=True)
+X0 = np.array([10, 5])  # initial conditions: 10 rabbits and 5 foxes
+X, infodict = integrate.odeint(lambda x, _: dX_dt(x, a, b, c, d), X0, t, full_output=True)
 
 print(X)
