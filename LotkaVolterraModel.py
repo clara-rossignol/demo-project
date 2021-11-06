1c0300707293b6bf1a82ce2e9be295191a619005
AlexEMG <alexander@deeplabcut.org>
2021-11-06 19:00:18 +0100

    Renamed library, tested functions, finalized docstrings

--- LotkaVolterraModel.py
+++ LotkaVolterraModel.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def dx_dt(X, a, b, c, d):
+def dX_dt(X, a, b, c, d):
     """
     Functions to compute the growth rate of fox and rabbit populations.
     
@@ -28,19 +28,25 @@ def dx_dt(X, a, b, c, d):
 
 def d2X_dt2(X, a=1.0, b=0.1, c=1.5, d=0.75):
     """
-    Compute the Jacobian matrix evaluated in X.
+    Compute the Jacobian matrix evaluated at X.
 
     Parameters
     ----------
 
     X : array
         Vector state of populations (rabbits, foxes)
-
-    TODO: Parameters a,b,c,d
+    a : float, optional
+        natural growth rate of the prey (rabbit)
+    b : float, optional
+        natural dying rate of the prey
+    c : float, optional
+        natural growth rate of the predator (fox)
+    d : float, optional
+        natural dying rate of the predator
 
     Returns
     -------
-    y : array, shape (2, 2)
+    numpy array, shape (2, 2)
         Array containing Jacobian of dX_dt
 
 
@@ -55,9 +61,25 @@ def d2X_dt2(X, a=1.0, b=0.1, c=1.5, d=0.75):
 
 def population_equilibrium(a=1.0, b=0.1, c=1.5, d=0.75):
     """
-    Returns equilibrium points of dX_dt, i.e. where groth rate is equal to zero.
+    Returns equilibrium points of dX_dt, i.e. where growth rate is equal to zero.
 
-    TODO: Parameters a,b,c,d
+    Parameters
+    ----------
+
+    a : float, optional
+        natural growth rate of the prey (rabbit)
+    b : float, optional
+        natural dying rate of the prey
+    c : float, optional
+        natural growth rate of the predator (fox)
+    d : float, optional
+        natural dying rate of the predator
+
+    Returns
+    -------
+    list
+        List of points with dX_dt = 0.
+
     """
 
     return np.zeros(2), np.array([c / (d * b), a / b])
@@ -67,7 +89,19 @@ def check_equilibrium(a=1.0, b=0.1, c=1.5, d=0.75):
     """
     Checks if population_equilibrium returns fixpoints of dX_dt.
 
-    TODO: Parameters a,b,c,d
+    Parameters
+    ----------
+
+    a : float, optional
+        natural growth rate of the prey (rabbit)
+    b : float, optional
+        natural dying rate of the prey
+    c : float, optional
+        natural growth rate of the predator (fox)
+    d : float, optional
+        natural dying rate of the predator
+
+
     """
 
     equilibria = population_equilibrium(a=a, b=b, c=c, d=d)
