78a1f629846252fab6038a85bff63fa818faa3f2
AlexEMG <alexander@deeplabcut.org>
2021-11-06 17:53:07 +0100

    Formatting, fix typos

--- functions.py
+++ functions.py
@@ -5,7 +5,8 @@ def dX_dt(X, a, b, c, d):
     """ Return the growth rate of fox and rabbit populations. """
     return np.array([a * X[0] - b * X[0] * X[1], -c * X[1] + d * b * X[0] * X[1]])
 
-def d2X_dt2(X, a=1.0, b=.1, c=1.5, d=.75):
+
+def d2X_dt2(X, a=1.0, b=0.1, c=1.5, d=0.75):
     """
     Compute the Jacobian matrix evaluated in X.
 
@@ -29,30 +30,29 @@ def d2X_dt2(X, a=1.0, b=.1, c=1.5, d=.75):
     >>> jacobian = d2X_dt2(np.zeros(2))
 
     """
-    return np.array([[a -b*X[1],   -b*X[0]     ],
-                    [b*d*X[1] ,   -c +b*d*X[0]] ])
+    return np.array([[a - b * X[1], -b * X[0]], [b * d * X[1], -c + b * d * X[0]]])
 
 
-def population_equilibrium(a=1.0, b=.1, c=1.5, d=.75):
+def population_equilibrium(a=1.0, b=0.1, c=1.5, d=0.75):
     """
     Returns equilibrium points of dX_dt, i.e. where groth rate is equal to zero.
 
     TODO: Parameters a,b,c,d
     """
 
-    return np.zeros(2),np.array([c/(d*b),a/b])
+    return np.zeros(2), np.array([c / (d * b), a / b])
 
 
-def check_equilibrium(a=1.0, b=.1, c=1.5, d=.75):
-    '''
+def check_equilibrium(a=1.0, b=0.1, c=1.5, d=0.75):
+    """
     Checks if population_equilibrium returns fixpoints of dX_dt.
 
     TODO: Parameters a,b,c,d
-    '''
+    """
 
-    equilibria = population_equilibrium(a=a,b=b,c=c,d=d)
-    for eq in equilibrium:
-        if np.all(dX_df(eq,a=a,b=b,c=c,d=d)):
+    equilibria = population_equilibrium(a=a, b=b, c=c, d=d)
+    for eq in equilibria:
+        if np.all(dX_dt(eq, a=a, b=b, c=c, d=d) == np.zeros(2)):
             print(eq, " is a fix point!")
         else:
             print(eq, " is not a fix point!")
