bfe1d5d42de7d6b920ec94add9bb0bc3caf9c0c6
AlexEMG <alexander@deeplabcut.org>
2021-11-21 22:34:20 +0100

    Testing value error

--- LotkaVolterraModel.py
+++ LotkaVolterraModel.py
@@ -37,6 +37,9 @@ def dX_dt(X, a=1.0, b=0.1, c=1.5, d=0.75):
     >>> dX_dt(np.zeros(2),1,0.1,1.5,.75)        # zero is a fixpoint
     array([0., 0.])
     """
+    if np.size(X)>2:
+        raise ValueError("X has only two dimensions!")
+    
     return np.array([a * X[0] - b * X[0] * X[1], -c * X[1] + d * b * X[0] * X[1]])
 
 
